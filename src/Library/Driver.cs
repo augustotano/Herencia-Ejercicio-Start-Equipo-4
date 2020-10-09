62eb159d750f8bea8ca2bc17583c5f5554338e30
Gonzalo Barreiro <barreirogonzalo@outlook.com>
2020-10-08 18:38:28 -0700

    Updated Person constructor with FaceFound method

--- src/Library/Driver.cs
+++ src/Library/Driver.cs
@@ -5,14 +5,33 @@ namespace Library
     public class Driver : Person
     {
 
-        public string Bio {get; set;}
         public string Vehicle {get; set;}
 
         //le sume una imagen para poder hacer lo de las caras --- vale
-        public Driver (string name, string surname, string CI, string bio, string vehicle, string image) : base(name, surname, CI, image)
+        public Driver (string name, string surname, string CI, string bio, string vehicle, string image, CognitiveFace cog) : base(name, surname, CI, bio, image, cog)
         {
             this.Vehicle = vehicle;
-            this.Bio = bio;
-        }   
+        }
+
+        public override void FoundFace(CognitiveFace cog)
+        {
+            if (cog.FaceFound)
+            {
+                Console.WriteLine("Face Found!");
+                if (cog.SmileFound)
+                {
+                    Console.WriteLine("Found a Smile :)");
+                    new SendTwitt().Send(this.Bio, this.Image);
+                }
+                else
+                {
+                    Console.WriteLine("No smile found :(");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No Face Found");
+            }
+        }
     }
 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,55 +8,13 @@ namespace Program
     {
         static void Main(string[] args)
         {
-
-            Person gus = new Passenger("Augusto","Taño","1234567-8","@bill.jpg");
-            Person gonza = new Passenger("Gonzalo","Barreiro","7894561-2","@bill.jpg");
-            Person rafa = new Passenger("Rafael","Duran","4561275-8","@bill.jpg");
-
-            Driver vale = new PoolDriver("Valentina","Alvarez","5231590-4","Hola soy Vale, recien me uni a UcuRide","Citroen C3 - Rojo",3,"@bill2.jpg");
+            CognitiveFace cog = new CognitiveFace("620e818a46524ceb92628cde08068242", true);
             
-            CognitiveFace cog = new CognitiveFace("a36648d3c5134ab692acd35605d491f7", true);
-            cog.Recognize(vale.Image);
-            CorrectImage.FoundDriversFace(cog, vale);
+            Person gus = new Passenger("Augusto","Taño","1234567-8",@"bill.jpg",cog);
+            Person gonza = new Passenger("Gonzalo","Barreiro","7894561-2",@"bill.jpg",cog);
+            Person rafa = new Passenger("Rafael","Duran","4561275-8",@"bill.jpg",cog);
 
-            //todas las imagenes son las mismas si quieren vemos de usar el barco para que de error 
-            cog.Recognize(gus.Image);
-            CorrectImage.FoundPassangersFace(cog, gus);
-            cog.Recognize(gonza.Image);
-            CorrectImage.FoundPassangersFace(cog,gonza);
-            cog.Recognize(rafa.Image);
-            CorrectImage.FoundPassangersFace(cog, rafa);
-            
-            //faltan los puntajes
-           
-            /*
-            En éste método deberas mostrar un ejemplo de funcionamiento de tu programa an pseudocódigo. A continuación te 
-            planteamos un ejemplo de como hacerlo. Esto no significa que te limites a hacer solamente esto, debes pensar 
-            en grande!
-
-            Usuario pasajero1 = nuevo Pasajero()
-            Usuario pasajero2 = nuevo Pasajero()
-            Usuario pasajero3 = nuevo Pasajero()
-            Usuario conductor1 = nuevo Conductor()
-            Usuario conductorPool1 = nuevo ConductorPool(maxPasajeros = 3)
-            UcuRideShare rideShare = nuevo UcuRideShare()
-            
-            rideShare.Add(conductor1)
-            Se publica en Twitter un nuevo conductor!
-
-            rideShare.Add(conductorPool1)
-            Se publica en Twitter un nuevo conductor!
-            
-            rideShare.Add(pasajero1)
-            Se publica en Twitter nuevo registro de pasajero!
-            
-            rideShare.Add(pasajero2)
-            Se publica en Twitter nuevo registro de pasajero!
-
-            rideShare.Add(pasajero3)
-            Se publica en Twitter nuevo registro de pasajero!
-
-            */
+            Driver vale = new Driver("Valentina","Alvarez","9876543-2","Hola soy Vale","Suzuki Swift - Blue",@"claramente-no-es-bill.jpg",cog);
         }
     }
 }
